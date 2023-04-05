e9433959e6164c3e85a0a3a3c909edde141d2a23
Sheida Khodabakhshian <sheidakh@my.yorku.ca>
2023-04-05 12:05:32 -0400

    Removed dead code and unused import statements from UserMealPlanner

--- src/presentation/UserMealPlanner.java
+++ src/presentation/UserMealPlanner.java
@@ -17,16 +17,12 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
-import com.toedter.calendar.JDateChooser;
-import com.toedter.calendar.JDayChooser;
 
 import businessLogic.UserActivity;
 import objects.Planner;
@@ -35,12 +31,8 @@ import objects.User;
 import persistence.DatabaseAccess;
 import persistence.UsersDAO;
 
-import com.toedter.calendar.JCalendar;
 import javax.swing.JTextArea;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.JScrollPane;
-import java.awt.List;
 import javax.swing.JList;
 
 public class UserMealPlanner {
@@ -55,7 +47,7 @@ public class UserMealPlanner {
 	DatabaseAccess access = new DatabaseAccess();
 	UsersDAO db = access.usersDB();
 	DefaultListModel<String> model = new DefaultListModel<String>();	
-	JList list = new JList();
+	JList<String> list = new JList<String>();
 	HashMap<String, Planner> p = UserActivity.currentUser.getWeekPlanner();
 
 
@@ -184,7 +176,7 @@ public class UserMealPlanner {
 		comboBox_1_2.setBounds(414, 141, 82, 30);
 		panel_recipeplanning.add(comboBox_1_2);
 		
-		JComboBox comboBox_4 = new JComboBox();
+		JComboBox<String> comboBox_4 = new JComboBox<String>();
 		comboBox_4.setBounds(210, 75, 128, 22);
 		panel_recipeplanning.add(comboBox_4);
 		comboBox_4.addItem("Select");
@@ -192,14 +184,14 @@ public class UserMealPlanner {
 			comboBox_4.addItem(r.getName());
 		}
 		
-		JComboBox comboBox_4_1 = new JComboBox();
+		JComboBox<String> comboBox_4_1 = new JComboBox<String>();
 		comboBox_4_1.setBounds(210, 112, 128, 22);
 		panel_recipeplanning.add(comboBox_4_1);
 		comboBox_4_1.addItem("Select");
 		for(Recipes r: db.getRecipes(UserActivity.currentUser) ) {
 			comboBox_4_1.addItem(r.getName());
 		}
-		JComboBox comboBox_4_2 = new JComboBox();
+		JComboBox<String> comboBox_4_2 = new JComboBox<String>();
 		comboBox_4_2.setBounds(210, 142, 128, 22);
 		panel_recipeplanning.add(comboBox_4_2);
 		comboBox_4_2.addItem("Select");
@@ -207,14 +199,14 @@ public class UserMealPlanner {
 			comboBox_4_2.addItem(r.getName());
 		}
 		
-		JComboBox comboBox_4_3 = new JComboBox();
+		JComboBox<String> comboBox_4_3 = new JComboBox<String>();
 		comboBox_4_3.setBounds(210, 178, 128, 22);
 		panel_recipeplanning.add(comboBox_4_3);
 		comboBox_4_3.addItem("Select");
 		for(Recipes r: db.getRecipes(UserActivity.currentUser) ) {
 			comboBox_4_3.addItem(r.getName());
 		}		
-		JComboBox comboBox_4_4 = new JComboBox();
+		JComboBox<String> comboBox_4_4 = new JComboBox<String>();
 		comboBox_4_4.setBounds(210, 215, 128, 22);
 		panel_recipeplanning.add(comboBox_4_4);
 		comboBox_4_4.addItem("Select");
@@ -239,9 +231,6 @@ public class UserMealPlanner {
 				RecipeCollection.recipeView =1;
 				RecipeCollection collection = new RecipeCollection();
 				collection.setVisible(true);
-//				frame.setVisible(false);
-//				Window win = SwingUtilities.getWindowAncestor(frame.getContentPane());
-//				win.dispose();
 			}
 		});
 		btnAddSaved.setFont(new Font("Tahoma", Font.PLAIN, 11));
@@ -337,7 +326,6 @@ public class UserMealPlanner {
 				boolean add = true;
 				User currentUser = UserActivity.currentUser;
 				for(Recipes recipe: db.getRecipes(UserActivity.currentUser)) {
-					//System.out.println(recipe.getName());
 					if(recipe.mealTime.contains(time)) {
 						if (currentUser.getUserAllergies().getAllergies().get("Eggs") !=null && currentUser.getUserAllergies().getAllergies().get("Eggs")==1 && (recipe.getIngredients().toLowerCase().contains("egg"))){
 							add =false;
